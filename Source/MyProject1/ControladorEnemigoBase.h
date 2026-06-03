00d22297a880385ada2569583d4cdd8926991ad7
Alej673 <alejandrolarco2020@hotmail.com>
2026-06-03 17:44:13 -0500

    Conversión a C++ y creación de ControladorEnemigoBase

---
+++ Source/MyProject1/ControladorEnemigoBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ControladorEnemigoBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT1_API AControladorEnemigoBase : public AAIController
+{
+	GENERATED_BODY()
+	
+};
